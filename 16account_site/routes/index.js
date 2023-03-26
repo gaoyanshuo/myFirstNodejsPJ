9c8537f42e9aab3f84f87f1a41dc1f918e13c208
gaogao <gaoyanshuo9388@yahoo.com>
2023-03-26 22:56:50 +0900

    account create & show list

--- 16account_site/routes/index.js
+++ 16account_site/routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync(__dirname + '/../data/data.json');
+const db = low(adapter);
+const shortid = require('shortid');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -8,14 +13,23 @@ router.get('/', function (req, res, next) {
 
 // 账单一览
 router.get('/account/', (req, res, next) => {
-  res.render('account');
+  const account_list = db.get('accounts').value();
+  console.log('account_list', account_list);
+  res.render('account', {account_list});
 });
 
 // add account
 router.post('/account/', (req, res, next) => {
+  // get request body
   const body = req.body;
+  // generate db id
+  const id = shortid.generate();
   console.log('body', body);
-  res.send('add account');
+  // save date
+  db.get('accounts')
+    .unshift({ id, ...body })
+    .write();
+  res.render('success', { success_msg: 'add successed!', url: '/account' });
 });
 
 // create account
