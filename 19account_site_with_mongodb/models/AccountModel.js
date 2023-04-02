02af9ee290464a0eefc8c5de27b99ec92b1df39e
gaogao <gaoyanshuo9388@yahoo.com>
2023-04-02 19:59:29 +0900

    modify_account_model_schema

--- 19account_site_with_mongodb/models/AccountModel.js
+++ 19account_site_with_mongodb/models/AccountModel.js
@@ -1,9 +1,19 @@
 const {mongoose} = require('mongoose')
 const accountSchema = mongoose.Schema({
-  "title": String,
-  "time": "2023-03-26",
-  "type": Number,
-  "account": Number,
+  "title": {
+    type: String,
+    required: true
+  },
+  "time": Date,
+//  収入＆支出
+  "type": {
+    type: Number,
+    default: -1
+  },
+  "account": {
+    type: Number,
+    required: true
+  },
   "remarks": String
 })
 const accountModel = mongoose.model('account', accountSchema)
